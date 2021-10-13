a547a823666dc10e83279e1c43436f83232cd3be
Haensele <92301437+Haensele@users.noreply.github.com>
2021-10-13 14:31:18 +0200

    Enter

---
+++ Übung1.js
@@ -0,0 +1,24 @@
+let Name1 = "Kevin";
+const König = "Noah W.";
+let Name2 = "Lukas";
+let Name3 = "Mario";
+let Name4 = "Azubi1";
+let Name5 = "Azubi2";
+let Name6 = "Azubi3";
+let Name7 = "Azubi4";
+let Name8 = "Azubi5";
+let Name9 = "Azubi6";
+
+let user = {
+    name: "Hans",
+    age: 20
+}
+
+const Tier = {
+    model: "Löwe",
+    Gefahr: 0
+}
+
+Name6 = "Peter Maffay";
+Name7 = "Jürgen Klopp";
+Name8 = "Jens without any frends";
