31f0756e337e3d586158ab5bea9e6a3c98149453
Haensele <92301437+Haensele@users.noreply.github.com>
2021-10-13 15:53:22 +0200

    Übung4

---
+++ Übung4.js
@@ -0,0 +1,17 @@
+const names = ["Lukas", "Noah", "Mario", "Kevin"];
+
+const mappedNames =names.map((name) => name.toUpperCase());
+console.log(mappedNames); 
+
+names.push("Stefan");
+
+console.log(names);
+
+names.pop();
+
+console.log(names);
+
+names.reverse();
+
+console.log(names);
+
