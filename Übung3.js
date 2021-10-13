f798ebb40b7f7f4c3224821e10d923b870b8765e
Haensele <92301437+Haensele@users.noreply.github.com>
2021-10-13 15:11:56 +0200

    Übung3

---
+++ Übung3.js
@@ -0,0 +1,28 @@
+function subtrahiereohnestudiere(number) {
+    return number - number; 
+}
+
+console.log(subtrahiereohnestudiere(5));
+
+
+const subtrahieren = function(number) {return number - number};
+
+console.log(subtrahieren(5));
+
+
+const anonym = (a, b) => a - b; 
+
+console.log(anonym(24, 14));
+
+
+function addieren(a, subtrahieren) {
+
+    return a + subtrahieren(5);
+    
+}
+
+
+
+
+
+
