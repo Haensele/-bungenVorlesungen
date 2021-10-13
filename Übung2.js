4451641a29164c53c6ad1157b7bbcd0eeed8aa2f
Haensele <92301437+Haensele@users.noreply.github.com>
2021-10-13 14:49:07 +0200

    enter

---
+++ Übung2.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 26; i++) {
+
+    console.log(i);
+
+}
+
+const user = {
+    name: "Kevin",
+    age: 25, 
+    gender: "m",
+
+}
+
+for (const i in user) {
+
+    console.log(i);
+
+}
+
+
+while(true) {
+    let i = 1; 
+    console.log(i);
+    i++;
+}
+
