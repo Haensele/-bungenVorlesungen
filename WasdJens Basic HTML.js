74dd85ea0580d4843a3b3ac200a1640adb5add02
Haensele <92301437+Haensele@users.noreply.github.com>
2021-10-14 15:31:21 +0200

    Änderung um /Lukas

--- WasdJens Basic HTML.js
+++ WasdJens Basic HTML.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/Lukas', (req, res) => {
+    console.log('Got a GET request on "/Lukas" ' );
+    res.send('Lukas Bade sitzt neben mir!');
+});
+
 app.listen(port, () => {
     console.log('Running...');
 });
